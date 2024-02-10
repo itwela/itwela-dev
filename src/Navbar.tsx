e5aaf8aae4ef1509c74236bb8897eaffb34ca5e9
Itwela Ibomu <36617380+itwela@users.noreply.github.com>
2024-02-10 00:28:13 -0500

    fixed padding

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -100,7 +100,7 @@ const Navbar = () => {
   return (
     <nav ref={navContainer} id='menu-wrapper' className='w-[100vw] fixed z-[2] flex place-items-starht'>
       {/* <ul onMouseOver={handleIsHoveredEnter} onMouseOut={handleIsHoveredExit} className='nav-bar-container items-center text-[0.7em] bottom-[3%] left-2 fixed z-[10] md:text-[1em] flex gap-4 md:gap-8 p-2 backdrop-blur rounded-[2em] w-[100vw] justify-evenly'> */}
-      <ul id="menu-cont" className='nav-bar-container items-start p-8 z-[10] md:text-[1em] flex flex-col gap-4 md:gap-8 p-2 rounded-[2em] w-[100vw] justify-evenly'>
+      <ul id="menu-cont" className='nav-bar-container items-start p-6 z-[10] md:text-[1em] flex flex-col gap-4 md:gap-8 p-2 rounded-[2em] w-[100vw] justify-evenly'>
         <li id='menu-button' onMouseDown={handleMenuClick} className='cursor-pointer p-2 absolute z-[2]'>Menu</li>
         <li id='home'><NavLink to="/" onClick={handleMenuClick} className='p-3 text-[3em]'>Home</NavLink></li>
         <li id='projects'><NavLink to="/Projects" onClick={handleMenuClick}  className='p-3 text-[3em]'>Projects</NavLink></li>
