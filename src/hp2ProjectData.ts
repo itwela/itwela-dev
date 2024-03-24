617e19c317e9421b360286c5f42c1e2d4bd4e0d2
Itwela Ibomu <36617380+itwela@users.noreply.github.com>
2024-03-24 16:24:58 -0400

    switched project order

--- src/hp2ProjectData.ts
+++ src/hp2ProjectData.ts
@@ -16,22 +16,22 @@ const projects: Project[] = [
     {
         id: 1,
         date: '2024',
-        name: "Zentask",
-        description: "A full-stack web application that helps users keep track of big to small tasks & projects in their life. \n This project is being built because of a coding workshop I'm attending to showcase my skills and land an interview with Cathie Stoscup (a connection I made on LinkedIn) ",
-        imageUrl: img3,
-        projectLink: "https://miro.com/app/board/uXjVNg5bmhE=/?share_link_id=246358243262",
-        status: 'In Progress',
-        toolsUsed: ['Next.js 14 ', 'PostgreSQL ', 'TailwindCSS ', 'Prisma ', 'Clerk Auth ', 'Material UI', 'Chakra UI' ],
-    },
-    {
-        id: 2,
-        date: '2024',
         name: "JobKompass",
         description: "A full-stack web application that helps users gain key insights into their job search journey.",
         imageUrl: img1,
         projectLink: "https://www.myjobkompass.com/",
         status: 'New',
         toolsUsed: ['Next.js 14 ', 'PostgreSQL ', 'TailwindCSS ', 'Prisma ', 'Clerk Auth ', 'Stripe ']
+    },
+    {
+        id: 2,
+        date: '2024',
+        name: "Zentask",
+        description: "A full-stack web application that helps users keep track of big to small tasks & projects in their life. \n This project is being built because of a coding workshop I'm attending to showcase my skills and land an interview with Cathie Stoscup (a connection I made on LinkedIn). ",
+        imageUrl: img3,
+        projectLink: "https://miro.com/app/board/uXjVNg5bmhE=/?share_link_id=246358243262",
+        status: 'In Progress',
+        toolsUsed: ['Next.js 14 ', 'PostgreSQL ', 'TailwindCSS ', 'Prisma ', 'Clerk Auth ', 'Material UI', 'Chakra UI' ],
     },
     {
         id: 3,
