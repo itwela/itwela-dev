d9883de8cdd2b6c17bd22ada6184ab51f1d1a783
Itwela Ibomu <36617380+itwela@users.noreply.github.com>
2024-03-25 07:37:51 -0400

    added more hoempage updates

--- client/src/hp4.tsx
+++ client/src/hp4.tsx
@@ -5,6 +5,9 @@ import homeicon from './assets/home.png'
 import { useRef } from 'react';
 import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
+import { SiLeetcode } from "react-icons/si";
+import { LiaHackerrank } from "react-icons/lia";
+
 
 
 
@@ -26,18 +29,25 @@ const HomepageSecFour = () => {
                         
                       <div className='w-full h-[50vh] flex flex-col gap-5 place-content-center place-items-start'>
                             <h2 className='text-6xl flex  gap-3 flex-wrap  place-items-center'>Check out <NavLink to="/slam"><span className='font-bold text-black'>S.L.A.M.</span></NavLink></h2>
-                            <div className='flex flex-col place-items-start'> 
-                              <h2 className=''>A Tool I am using to help me master <span className='font-bold'>Leetcode</span>.</h2>
-                              <h2 className='flex gap-2 flex-wrap place-content-start'>
+                            <div className=''> 
+                              <div className=' flex flex-col place-content-center'>
+                                <h2 className=''>A Tool I am using to help me master platforms like</h2>
+                                 <span className='flex gap-2 py-2'>
+                                   <a href="https://leetcode.com/itwela/" target='_blank'><span className='font-bold flex gap-1'>Leetcode <SiLeetcode /></span></a>
+                                   and
+                                   <a href="https://www.hackerrank.com/profile/deanandnostrand" target='_blank'><span className='font-bold flex gap-1'>HackerRank <LiaHackerrank />.</span></a>
+                                 </span>
+                              </div>
+                              <div className='flex place-content-start '>
                                   
-                                  <span className='flex  gap-2 flex-wrap py-2'>
+                                  <span className='flex  gap-2 flex-wrap'>
                                     <span><span className='font-bold'>S</span> - Structures</span>
                                     <span><span className='font-bold'> L</span> - Lexicon,</span>
                                     <span><span className='font-bold'> A </span>- Algorithms,</span>
                                     <span><span className='font-bold'> M</span> - Methods</span>
                                   </span>
                                 
-                                </h2>
+                                </div>
                             </div>
                       </div>
 
